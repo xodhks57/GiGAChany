120472eb5b4203a304f33778a43ffdbf12038a44
xodhks57 <xodhks57@naver.com>
2019-09-25 21:02:22 +0900

    backup

--- User/MS/test.py
+++ User/MS/test.py
@@ -8,7 +8,7 @@ face_classifier = cv2.CascadeClassifier('/home/gigachany/anaconda3/envs/gigachan
 def face_detector(img, size = 0.5):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray,1.3, 5)
-    if faces is ():
+    if faces is () :
         return None 
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 255), 2)
@@ -17,8 +17,6 @@ def face_detector(img, size = 0.5):
     return img, face   
 
 def Gender_Checker(image, number):
-    if number == 0 : print("Female Check")
-    else : print("Male Check")
     img, face = face_detector(image)
     ModelName = ["human_female.yml", "human_male.yml"]
     try:
@@ -26,7 +24,7 @@ def Gender_Checker(image, number):
         model = cv2.face.LBPHFaceRecognizer_create()
         model.read("/home/gigachany/CODE/model/"+ModelName[number])
         result = model.predict(face)
-        if result[1] < 500:
+        if result[1] < 500 :
             confidence = int(100 * (1 - (result[1]) / 300))
             display_string = str(confidence) + '%'
         print("Check Complete")
@@ -38,10 +36,11 @@ def Gender_Checker(image, number):
 def Checker_Output(A, B):
     if A > B : print("삐빅! 사진의 인물은 여성입니다.")
     elif A < B : print("삐빅! 사진의 인물은 남성입니다.")
-    else : print("삐빅! 사람이 아닙니다.")
+    else : print("삐빅! 반반입니다.")
 
 if __name__ == "__main__":
     path = '/home/gigachany/CODE/GiGAChany/SendImage/images.jpg'
     image = cv2.imread(path)
+    print("Wait..")
     Checker_Output(Gender_Checker(image, 0), Gender_Checker(image, 1))
 
