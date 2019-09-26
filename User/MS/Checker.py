d002d3542e200cb29ce0e76cf3322d6e6904881d
xodhks57 <xodhks57@naver.com>
2019-09-26 21:33:02 +0900

    서버 클라이언트
    ㅎㅇㅎㅇ

--- User/MS/Checker.py
+++ User/MS/Checker.py
@@ -56,30 +56,19 @@ def Checker_Output(A, B):
     elif A < B : return str(B + '^male')
     else : return str('0^not')
 
-def recvall(sock, count):
-    buf = b''
-    while count:
-        newbuf = sock.recv(count)
-        if not newbuf: return None
-        buf += newbuf
-        count -= len(newbuf)
-    return buf
-
 def Server():
-    TCP_IP = 'localhost'
-    TCP_PORT = 9055
+    TCP_IP = '10.10.20.54'
+    TCP_PORT = 9097
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((TCP_IP, TCP_PORT))
     s.listen(True)
-    conn, addr = s.accept()
-    length = recvall(conn,16)
-    stringData = recvall(conn, int(length))
-    data = np.fromstring(stringData, dtype='uint8')
-    decimg=cv2.imdecode(data,1)
-    cv2.imwrite('./test.jpg',decimg)
-    print("이미지 저장 완료");
+    s.accept()
+    img = open("/home/gigachany/CODE/GiGAChany/SendImage/images.jpg","wb")
+    data = s.recv(1024)
+    img.write(data)
+    img.close()
+    print("이미지 저장 완료")
     s.close()
-
 
 if __name__ == "__main__":
     Server();
