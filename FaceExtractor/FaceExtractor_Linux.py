d291725c4b61b752ec4fb5e82da1ba59cf85b302
xodhks57 <xodhks57@naver.com>
2019-09-27 19:39:53 +0900

    update data male
    update data

--- FaceExtractor/FaceExtractor_Linux.py
+++ FaceExtractor/FaceExtractor_Linux.py
@@ -5,20 +5,23 @@ import numpy as np
 face_classifier = cv2.CascadeClassifier('/home/gigachany/anaconda3/envs/gigachany/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
 
 def face_extractor(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_classifier.detectMultiScale(gray,1.3, 5)
-    if faces is ():
-        return None 
-    for (x, y, w, h) in faces:
-        cropped_face = img[y:y + h, x:x + w]
-    return cropped_face   
+    try : 
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_classifier.detectMultiScale(gray,1.3, 5)
+        if faces is ():
+            return None 
+        for (x, y, w, h) in faces:
+            cropped_face = img[y:y + h, x:x + w]
+        return cropped_face 
+    except:
+        pass  
 
 def face_Data(name):
     img = cv2.imread(name)
     if face_extractor(img) is not None:
         face = cv2.resize(face_extractor(img), (200, 200))
         face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
-        file_name_path = '/home/gigachany/CODE/GiGAChany/data/data_female/f' + str(count) + '.jpg'
+        file_name_path = '/home/gigachany/CODE/GiGAChany/data/data_male/m' + str(count) + '.jpg'
         cv2.imwrite(file_name_path, face)
         cv2.putText(face, str(count), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
         cv2.imshow('Face Cropper', face)
